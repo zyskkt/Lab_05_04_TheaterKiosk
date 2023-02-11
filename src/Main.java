d3f0f07510a0db3c77aee8657b728ce5f921a637
Kent Zysk <kent@kentzysk.com>
2023-02-11 01:29:38 -0600

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Init variable
+        int age = 20;
+        //Ask for their age
+        System.out.println("What is your age?");
+            //If they are over 21, let them know they get a wristband
+            if (age >= 21) {
+                System.out.println("You get a wristband!");
+            }
     }
 }
